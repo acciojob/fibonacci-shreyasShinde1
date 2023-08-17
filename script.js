773eb1f34d11707116b288c04f9ee4eb249f2f15
Shreyas Shinde <130788791+shreyasShinde1@users.noreply.github.com>
2023-08-17 23:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	if(num === 0){
+		return 0;
+	}
+	else if(num === 1){
+		return 1;
+	}
+
+	let prevNum = 0;
+	let currNum = 1;
+	for(let i = 2; i < num; i++){
+		const temp = currNum;
+		currNum = prevNum + currNum
+		prevNum = temp;
+	}
+
+	return currNum;
 }
 
 module.exports = fibonacci;
